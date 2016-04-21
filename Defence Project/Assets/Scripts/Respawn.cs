e3eb3896c87b52f768f344211d44f977df6224cf
서보경 <totoru1219@gmail.com>
2016-04-22 02:19:47 +0900

    Defence Project 기본적인 기능 구현
    10x10 블럭을 이동하는 Enemy 구현
    
    시간마다 Missile 발사하는 Ahchor 구현. ( 적 충돌시 체력감소 & 미사일 제거)

---
+++ Defence Project/Assets/Scripts/Respawn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+    public GameObject monster;
+    public float spawnTime = 1f;
+    public float repeatCount = 10f;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+    void Spawn()
+    {
+        Instantiate(monster, gameObject.transform.position, gameObject.transform.rotation);
+
+        repeatCount--;
+        if (repeatCount <= 0)
+        {
+            CancelInvoke("Spawn");
+        }
+    }
+
+}
---
+++ Defence Project/Assets/Scripts/WaypointRoot.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointRoot : MonoBehaviour {
+    public Transform[] GetWayPoints()
+    {
+        return gameObject.GetComponentsInChildren<Transform>();
+    }
+}
