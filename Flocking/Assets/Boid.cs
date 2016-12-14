eae9fd4af03b378c619426967a99953a60ca53d4
서보경 <totoru1219@gmail.com>
2016-12-14 14:17:26 +0900

    Flocking
    Target을 따라다니는 Boid구현

---
+++ Flocking/Assets/Boid.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+    public BoidController controller;
+    
+    private Vector3 velocity;
+    private Vector3 dir;
+    public float moveSpeed;
+    public float rotateSpeed;
+
+    public float NEIGHBOUR_RADIUS;
+    public float DESIRED_SEPARATION;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Calc());
+    }
+	
+    private void FixedUpdate()
+    {
+        velocity = Vector3.Slerp(velocity, dir, rotateSpeed * Time.fixedDeltaTime);
+        transform.Translate(velocity * moveSpeed *Time.fixedDeltaTime);
+    }
+
+    IEnumerator Calc()
+    {
+        while (true)
+        {
+            Vector3 AvgPosition, AvgVelocity, AvgDistance;
+            AvgPosition = -transform.position;
+            AvgVelocity = -velocity;
+            AvgDistance = Vector3.zero;
+            int flockSize = -1;
+            int serpSize = -1   ;
+            if (controller.boids.Count > 0)
+            {
+                foreach (Boid boid in controller.boids)
+                {
+                    if (Vector3.Distance(transform.position, boid.transform.position) <= NEIGHBOUR_RADIUS)
+                    {
+                        AvgPosition += boid.transform.position;
+                        AvgVelocity += boid.velocity;
+                        flockSize++;
+                        float distance = Vector3.Distance(transform.position, boid.transform.position);
+                        if (distance <= DESIRED_SEPARATION)
+                        {
+                            Vector3 v = transform.position - boid.transform.position;
+                            v.Normalize();
+
+                            // 거리에 반비례하여 세기가 강함
+                            if (v.x != 0) v.x = distance / v.x;
+                            if (v.y != 0) v.y = distance / v.y;
+                            if (v.z != 0) v.z = distance / v.z;
+
+                            AvgDistance += v;
+                            serpSize++;
+                        }   
+                    }
+                }
+                if (flockSize > 0)
+                {
+                    AvgPosition = AvgPosition / flockSize - transform.position;
+                    AvgVelocity = AvgVelocity / flockSize;
+                    if (serpSize > 0) AvgDistance = AvgDistance / serpSize;
+                }
+
+                Vector3 follow = controller.target.position - transform.position;
+
+                dir = AvgDistance + AvgVelocity + AvgPosition + follow*0.01f;
+
+                dir.Normalize();
+            }
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, velocity);
+    }
+
+}
---
+++ Flocking/Assets/BoidController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidController : MonoBehaviour {
+    public Boid prefab;
+    public Transform target;
+
+    public int flockSize = 20;
+    public List<Boid> boids = new List<Boid>();
+
+    // Use this for initialization
+    void Awake()
+    {
+        for (int i = 0; i < flockSize; i++)
+        {
+            Boid boid = Instantiate(prefab, transform.position, transform.rotation) as Boid;
+            boid.transform.parent = transform;
+            boid.transform.localPosition = new Vector3(
+                            Random.value * GetComponent<Collider>().bounds.size.x,
+                            Random.value * GetComponent<Collider>().bounds.size.y,
+                            Random.value * GetComponent<Collider>().bounds.size.z) - GetComponent<Collider>().bounds.extents;
+            boid.controller = this;
+            boids.Add(boid);
+        }
+    }
+}
