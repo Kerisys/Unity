144d279e72eba6f38fc20943a895241ad337914a
서보경 <totoru1219@gmail.com>
2016-12-21 13:36:09 +0900

    모든 Branch 병합

---
+++ Inventory/Assets/Scripts/ItemManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemManager : MonoBehaviour {
+
+    public List<Sprite> imageList;
+    
+    public Sprite GetImage(int index)
+    {
+        return imageList[index];
+    }
+
+    public int Length()
+    {
+        return imageList.Count;
+    }
+}
